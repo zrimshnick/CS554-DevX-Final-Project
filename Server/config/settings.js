3c075dbe6e480aa45a5f3bdaf3bbcd6d36c4d438
ZackRimshnick <zrimshnick@gmail.com>
2024-11-18 16:42:26 -0500

    created Mongo cloud database to access where hosted

--- Server/config/settings.js
+++ Server/config/settings.js
@@ -1,4 +1,6 @@
 export const mongoConfig = {
-  serverUrl: "mongodb://localhost:27017/",
+  /* serverUrl: "mongodb://localhost:27017/", */
+  serverUrl:
+    "mongodb+srv://developer:passw0rd123!@devx-project.wrdgk.mongodb.net/",
   database: "DevX-Project-Database",
 };
